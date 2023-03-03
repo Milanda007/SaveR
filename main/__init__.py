4d13d3803f6abd1d156301e758e9595daabde2df
Milanda007 <96860196+Milanda007@users.noreply.github.com>
2023-03-03 18:57:26 +0330

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = 27590454
+API_HASH = "11bdf99a73d502d2df4f333b02fd2b2f"
+BOT_TOKEN = "6201704107:AAHyEywDMN1DP9aW_EeEWSQsOcDInrriaHo"
+SESSION = "AQB3R6Neih5YY6ZpmExttE6ljTN4GkUF3IJdWz_wbXVYDr1aX3Q65y6tKLzcvJRqQChy6D76QJEegwkDYZr8Ct-8wX7RVyMgTPWbgZv9UUoN0SiSibccTTFRlVgVliaNBBH-giUWPg9ypeyVJ6vizf0q99ApZEtFPYSQ2Z_yd0f1xObjNq9-NR1OS-JV6hUSoUXAkgyVZTHp2pXd8NOLR2sGZSHM15g4CCN1TduqjhoC2FMhmr9GVHIrDPQM2DcfGgGsQh93VnEVh1DbxhwekklTZVd6eF_cBIgNxLnmNmOnDnR2smOEK0wTZTjC9SsY56LK5gcDPdX7GmkuNuQCI9v0AAAAAS8Y3EIA"
+FORCESUB = "channelforwarded"
+AUTH = 5085125698
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
